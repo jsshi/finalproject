94b5f36f70fedcc6397aa635544cd06a3b2e2eea
= <=>
2012-12-06 03:37:54 -0500

    pages

--- templates/home.php
+++ templates/home.php
@@ -7,19 +7,58 @@
     </section>
 
     <section class="seven columns" align = "center" >
-        <?php foreach($rows as $row)
+        <?php
+            $perpage = 10;
+            
+            $numrows = count($rows);
+            
+            $last = ceil($numrows / $perpage);
+            
+            if (isset($_GET['page']))
+                $page = $_GET['page'];
+            if ($page < 1)
+                $page = 1;
+                
+            $start = ($page - 1) * $perpage;
+            $end = $start + $perpage - 1;
+            
+            if ($end > $numrows - 1)
+                $end = $numrows - 1;
+                
+            if ($page == 1)
+                echo "<< FIRST  < PREV ";
+            else
+            {
+                $prevpage = $pageno - 1;
+                echo " <a href='?page=1'> << FIRST</a> ";
+                echo "  <a href='?page=$prevpage'> < PREV</a> ";
+            }
+            
+            echo " ( Page $page of $last ) ";
+            
+            if ($page == $last)
+                echo " NEXT >  LAST >> ";
+            else
+            {
+                $nextpage = $page + 1;
+                echo " <a href='?page=$nextpage'>NEXT ></a>  ";
+                echo " <a href='?page=$lastpage'>LAST >></a> ";
+            }
+        echo "<p>";
+        
+        for($i = $start; $i <= $end; $i++)
         {
             print("<div class='row'>
-                <div class='nine columns'>
-                    <p><a href='#' data-reveal-id='recipe".$row["id"]."' class='button'>" . htmlspecialchars($row["title"]) . "</a></p>
-                    <p>" . htmlspecialchars($row["description"]) . "</p></div><hr/>
+                <div class='twelve columns'>
+                    <p><a href='#' data-reveal-id='recipe".$rows[$i]["id"]."' class='button'>" . htmlspecialchars($rows[$i]["title"]) . "</a></p>
+                    <p>" . htmlspecialchars($rows[$i]["description"]) . "</p></div><hr/>
             </div>
-            <div id='recipe".$row["id"]."' class='reveal-modal'>
-                <h3>" . htmlspecialchars($row["title"]) . "</h3>
-                <p><i>" . htmlspecialchars($row["description"]) . "</i></p>
-                <p>" . $row["ingredients"] . "</p>
-                <p>" . htmlspecialchars($row["instructions"]) . "</p>
-                <p>" . $row["tags"] . "</p>
+            <div id='recipe".$rows[$i]["id"]."' class='reveal-modal'>
+                <h3>" . htmlspecialchars($rows[$i]["title"]) . "</h3>
+                <p><i>" . htmlspecialchars($rows[$i]["description"]) . "</i></p>
+                <p>" . $rows[$i]["ingredients"] . "</p>
+                <p>" . htmlspecialchars($rows[$i]["instructions"]) . "</p>
+                <p>" . $rows[$i]["tags"] . "</p>
             <a class='close-reveal-modal'>x</a></div>");
         }
         ?>
